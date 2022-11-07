084eaf145afe07e3cab759cffb09c777f503a4e4
Zivotovskiy <https://github.com/ZhivotovskyMaksim>
2022-11-07 14:20:25 +0500

    init parth_2 web.starter

--- SpringQuickly/src/main/java/com/max/SpringQuicklyApplication.java
+++ SpringQuickly/src/main/java/com/max/SpringQuicklyApplication.java
@@ -1,8 +1,12 @@
 package com.max;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringQuicklyApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		SpringApplication.run(SpringQuicklyApplication.class, args);
 	}
 }
